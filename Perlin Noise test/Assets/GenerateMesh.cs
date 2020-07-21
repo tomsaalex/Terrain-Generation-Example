c5bcc63345804cec6e14842e2a7ac6e012d5da13
tomsaalex <tomsaalex@gmail.com>
2020-07-21 23:04:36 +0300

    Added project from PC

---
+++ Perlin Noise test/Assets/GenerateMesh.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateMesh : MonoBehaviour
+{
+    Mesh terrain;
+    Vector3[] vertices;
+    int[] triangles;
+
+    public int size = 100;
+
+    void Start()
+    {
+        terrain = new Mesh();
+        GetComponent<MeshFilter>().mesh = terrain;
+        vertices = new Vector3[size * size];
+        triangles = new int[size * size * 6];
+
+        CreateShape();
+        UpdateShape();
+    }
+
+    void CreateShape()
+    {
+        /*vertices = new Vector3[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 0, 1),
+            new Vector3(1, 0, 0)
+        };
+
+        triangles = new int[]
+        {
+            0, 1, 2
+        };*/
+
+        for(int z = 0; z < size ; z++)
+        {
+            for(int x = 0; x < size; x++)
+            {
+                vertices[z * size + x] = new Vector3(x, 0, z);
+                //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                //cube.transform.position = new Vector3(x, y, 0);
+            }
+        }
+
+        int verts = 0, tris = 0;
+
+        for (int x = 0; x < size - 1; x++)
+        {
+            for (int z = 0; z < size - 1; z++)
+            {
+                triangles[tris + 0] = 0 + verts;
+                triangles[tris + 1] = size + verts;
+                triangles[tris + 2] = 1 + verts;
+                triangles[tris + 3] = 1 + verts;
+                triangles[tris + 4] = size + verts;
+                triangles[tris + 5] = 1 + size + verts;
+                verts++;
+                tris += 6;
+            }
+            verts++;
+        }
+
+        for(int x = 0; x < size; x++)
+            for(int z = 0; z < size; z++)
+            {
+                ;
+            }
+    }
+
+    void UpdateShape()
+    {
+        terrain.Clear();
+
+        terrain.vertices = vertices;
+        terrain.triangles = triangles;
+    }
+}
---
+++ Perlin Noise test/Assets/Generate_Texture.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Generate_Texture : MonoBehaviour
+{
+    public int tex_width = 1000;
+    public int tex_height = 1000;
+
+    public float orgX = 1;
+    public float orgY = 1;
+
+    public float scale = 5.0f;
+
+    Renderer rend;
+    Texture2D noiseTexture;
+    Color[] pix;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+
+        noiseTexture = new Texture2D(tex_width, tex_height);
+        pix = new Color[noiseTexture.width * noiseTexture.height];
+        rend.material.mainTexture = noiseTexture;
+
+    }
+
+    public void CalculateNoise()
+    {
+        float y = 0;
+        
+        while (y < noiseTexture.height)
+        {
+            float x = 0;
+            while (x < noiseTexture.width)
+            {
+                float xCoord = orgX + x / noiseTexture.width * scale;
+                float yCoord = orgY + y / noiseTexture.height * scale;
+
+                float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                /*Color c;
+                if (sample < 0.2)
+                {
+                    c = new Color(0, 0, 50);
+                }
+                else if (sample < 0.4)
+                {
+                    c = new Color(0, 50, 0);
+                }
+                else if (sample < 0.6)
+                {
+                    c = new Color(0, 50, 50);
+                }
+                else if (sample < 0.8)
+                {
+                    c = new Color(50, 50, 0);
+                }
+                else
+                    c = new Color(50, 50, 50);*/
+                pix[(int)x + (int)y * noiseTexture.width] = new Color(Mathf.Sin(sample * 2), Mathf.Tan(sample/2), Mathf.Cos(4 * sample));
+                x++;
+            }
+            y++;
+        }
+        noiseTexture.SetPixels(pix);
+        noiseTexture.Apply();
+
+        var bytes = noiseTexture.EncodeToPNG();
+
+        var dirPath = Application.dataPath + "/Assets";
+        File.WriteAllBytes(dirPath + "-Texture.png", bytes);
+    }
+
+    
+}
